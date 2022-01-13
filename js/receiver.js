60925d29ca26985e27196c0fd92d8b3b1c9184bb
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-13 22:09:35 +0700

    Working on player UI

--- js/receiver.js
+++ js/receiver.js
@@ -20,6 +20,14 @@ timeline.style.setProperty('display', 'none');
 let debug = document.createElement('div');
 debug.style.cssText = 'position:absolute;left:0;right:0;top:2%;bottom:0;text-align:center;font-size:40px;color:red;';
 document.body.appendChild(debug);
+debug.innerHTML = "v1" // TODO
+
+context.getPlayerManager().setMessageInterceptor(
+  cast.framework.messages.MessageType.LOAD, loadRequestData => {
+    debug.innerHTML = "Load: " + loadRequestData.media.contentUrl; // TODO
+    return loadRequestData;
+  }
+);
 
 let video = playerRoot.querySelectorAll('.mediaElement')[0];
 video.addEventListener('loadedmetadata', () => {
