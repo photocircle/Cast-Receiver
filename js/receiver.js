b18476f26c7319452de9ff5f5db5c3158add4b0c
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-13 23:12:06 +0700

    Working on player UI

--- js/receiver.js
+++ js/receiver.js
@@ -20,11 +20,12 @@ timeline.style.setProperty('display', 'none');
 let debug = document.createElement('div');
 debug.style.cssText = 'position:absolute;left:0;right:0;top:2%;bottom:0;text-align:center;font-size:40px;color:red;';
 document.body.appendChild(debug);
-debug.innerHTML = "v2" // TODO
+
+let foreground = playerRoot.querySelectorAll('.foreground')[0];
 
 let preview = document.createElement('img');
 preview.style.cssText = 'position: absolute; height: 100%; width: 100%; object-fit: contain;';
-document.body.insertBefore(preview, document.body.firstChild);
+foreground.parentElement.insertBefore(preview, foreground);
 
 let video = playerRoot.querySelectorAll('.mediaElement')[0];
 // Disallow Chromecast to stop casting on the end
@@ -32,11 +33,12 @@ video.addEventListener('ended', event => {
   event.stopImmediatePropagation();
 }, true);
 
+let c = 0;
 context.getPlayerManager().setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD, loadRequestData => {
     preview.src = loadRequestData.media.contentId.replace("/photo/", "/thumb/");
     video.poster = loadRequestData.media.contentId.replace("/video/", "/thumb/");
-    return loadRequestData;
+    return if (c++ % 2 == 0) { loadRequestData } else { null };
   }
 );
 
