6a6dd097ec7817b698e5b4dfdb7a03098b77242e
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-13 18:08:15 +0700

    Working on player UI

--- js/receiver.js
+++ js/receiver.js
@@ -1,5 +1,14 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 
+
+let playPause = document.getElementsByClassName('controlsPlayPause')[0];
+playPause.style.setProperty('position', 'relative');
+playPause.style.setProperty('left', '50%');
+
+let timeline = document.getElementsByClassName('controlsTimeline')[0];
+timeline.style.setProperty('display', 'none');
+
+
 const playbackConfig = new cast.framework.PlaybackConfig();
 playbackConfig.autoPauseDuration = 0.5;
 playbackConfig.autoResumeDuration = 1.0;
