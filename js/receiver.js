6d5d7b8cd21ed7725fa6700ecf964040f942d074
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-13 17:29:30 +0700

    Working on player UI

--- js/receiver.js
+++ js/receiver.js
@@ -1,13 +1,7 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 
-
-let playerElement = document.getElementsByTagName("cast-media-player")[0];
-
 const playbackConfig = new cast.framework.PlaybackConfig();
 playbackConfig.autoPauseDuration = 0.5;
 playbackConfig.autoResumeDuration = 1.0;
-
-const controls = cast.framework.ui.Controls.getInstance();
-controls.clearDefaultSlotAssignments();
 
 context.start({playbackConfig: playbackConfig});
