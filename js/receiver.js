0ebfd642e2800811d460f53354ee31d4ba80a78b
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-13 17:00:39 +0700

    Changed logo and player behavior

--- js/receiver.js
+++ js/receiver.js
@@ -1,5 +1,13 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 
+
 let playerElement = document.getElementsByTagName("cast-media-player")[0];
 
-context.start();
+const playbackConfig = new cast.framework.PlaybackConfig();
+playbackConfig.autoPauseDuration = 0.5;
+playbackConfig.autoResumeDuration = 1.0;
+
+const controls = cast.framework.ui.Controls.getInstance();
+controls.clearDefaultSlotAssignments();
+
+context.start({playbackConfig: playbackConfig});
