0ce67b2c9b2564a7689a0f116f6340ac80b275a1
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-14 16:37:06 +0700

    Working on player UI

--- js/receiver.js
+++ js/receiver.js
@@ -2,9 +2,20 @@ import { showImage } from './imageviewer.js';
 
 const context = cast.framework.CastReceiverContext.getInstance();
 
+var prevTimeoutId = null;
+
 context.getPlayerManager().setMessageInterceptor(cast.framework.messages.MessageType.LOAD, loadRequestData => {
   showImage(loadRequestData.media.contentId);
+
+  // Custom idle timeout
+  if (prevTimeoutId) clearTimeout(prevTimeoutId);
+  prevTimeoutId = setTimeout(() => {
+    context.stop();
+  }, 5 * 60 * 1000);
+
   return null;
 });
 
+const options = new cast.framework.CastReceiverOptions();
+options.disableIdleTimeout = true;
 context.start();
