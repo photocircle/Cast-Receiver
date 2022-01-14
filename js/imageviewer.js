6db1d0e1ad24768a5baf93abd24cad2b79abc6e2
Alex Vasilkov <alexvasilkov@gmail.com>
2022-01-14 19:08:25 +0700

    Working on player UI

--- js/imageviewer.js
+++ js/imageviewer.js
@@ -6,13 +6,18 @@ let currentImg = null;
 function show(type, src) {
   srcMap.set(type, src);
   let img = new Image();
-  img.classList.add("img-" + type, "fullscreen");
+  img.classList.add("img", "fullscreen");
   img.onload = () => {
     if (src != srcMap.get(type)) return;
     srcMap.delete("thumb")  // Avoid showing thumb image, if full is already loaded
     if (currentImg) currentImg.remove();
     currentImg = img;
-    document.body.appendChild(img);
+    if (type == "thumb") {
+      // Small delay to make full image more chances to show up first
+      setTimeout(() => { if (currentImg == img) document.body.appendChild(img); }, 300);
+    } else {
+      document.body.appendChild(img);
+    }
   }
   img.onerror = () => {
     if (src != srcMap.get(type)) return
